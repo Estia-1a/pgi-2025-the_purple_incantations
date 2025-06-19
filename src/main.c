d5ea5ac93d3fa4ba67bff747afed966faee3fd26
vtellechea <189098663+vtellechea@users.noreply.github.com>
2025-06-19 16:27:37 +0200

    probème réglé

--- src/main.c
+++ src/main.c
@@ -68,16 +68,4 @@ int main(int argc, char **argv) {
 
   return 0;
 }
-void color_red(char *source_path){
-    unsigned char *data = NULL;
-    int width=0, height =0, channel_count=0;
-    int i ;
-    read_image_data(source_path, &data, &width, &height, &channel_count);
-    for(i=0; i<channel_count * width * height;i++){
-       if (i%3 != 0){
-        data[i] = 0;
-       }
-    }
-    write_image_data("./images/input/image_out.bmp", data, width, height);
-}
  
