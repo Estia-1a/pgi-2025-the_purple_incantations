fb7388158bf0ec45c42170c0ab49ea074f7497f0
vtellechea <189098663+vtellechea@users.noreply.github.com>
2025-06-19 14:05:26 +0200

    rotate acw validé

--- src/features.c
+++ src/features.c
@@ -88,51 +88,68 @@ void second_line (char *source_path){
     }
 }
 
-/*rotation sens horaire*/
-/*void rotate_cw(char *source_path){
+/*rotation sens anti-horaire*/
+void rotate_acw(char *source_path){
     unsigned char *datasrc = NULL;
-    int width=0, height =0, channel_count=0;
-    int x,y ;
-    read_image_data(source_path, &datasrc, &width, &height, &channel_count);
-    unsigned char *datadest= malloc(height*width*channel_count*sizeof(unsigned char));
- 
-    for (x=0;x<height;x++){
-        for (y=0; y<width; y++) {
-            set_pixel(datadest, height, width, channel_count, x, y, datasrc, y, width - 1 - x);
+    int width = 0, height = 0, channel_count = 0;
+
+    if (!read_image_data(source_path, &datasrc, &width, &height, &channel_count)) {
+        fprintf(stderr, "Erreur lors de la lecture de l'image.\n");
+        return;
+    }
+    int new_width = height;
+    int new_height = width;
+
+    unsigned char *datadest = malloc(new_width * new_height * channel_count * sizeof(unsigned char));
+    if (!datadest) {
+        fprintf(stderr, "Erreur d'allocation mémoire.\n");
+        free_image_data(datasrc);
+        return;
+    }
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            pixelRGB *pixel = get_pixel(datasrc, width, height, channel_count, x, y);
+            if (pixel) {
+                set_pixel(datadest, new_width, new_height, channel_count, y, new_height - 1 - x, pixel);
+            }
         }
     }
-    write_image_data("./images/input/image_rotatecw_out.bmp", datadest, width, height);
+
+    write_image_data("./images/input/image_rotatecw_out.bmp", datadest, new_width, new_height);
+
     free(datadest);
-}*/
+    free_image_data(datasrc);
+}
 
 void max_pixel(char *image_path) {
-    // Déclaration des variables
-    unsigned char *image = NULL;   // Contiendra les données de l'image
-    int width = 0, height = 0, channels = 0; // Dimensions et nombre de canaux (R, G, B)
+    
+    unsigned char *image = NULL;  
+    int width = 0, height = 0, channels = 0; 
 
-    // Charger l'image
+    
     read_image_data(image_path, &image, &width, &height, &channels);
 
-    // Variables pour garder le pixel avec la plus grande somme R+G+B
+    
     int max_sum = -1;
     int pixel_x = 0, pixel_y = 0;
     unsigned char max_r = 0, max_g = 0, max_b = 0;
 
-    // Parcourir tous les pixels de l'image
+    
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            // Calcul de l'index du pixel dans le tableau
+          
             int i = (y * width + x) * channels;
 
-            // Lire les 3 composantes du pixel
+            
             unsigned char r = image[i];
             unsigned char g = image[i + 1];
             unsigned char b = image[i + 2];
 
-            // Calculer la somme R + G + B
+            
             int sum = r + g + b;
 
-            // Si c'est plus grand que le max actuel, on le garde
+            
             if (sum > max_sum) {
                 max_sum = sum;
                 pixel_x = x;
@@ -144,7 +161,7 @@ void max_pixel(char *image_path) {
         }
     }
 
-    // Afficher le résultat
+
     printf("max_pixel (%d, %d): %d, %d, %d\n", pixel_x, pixel_y, max_r, max_g, max_b);
 }
 
